615a4f4fa5ea40c9680bf53e970656b524e0dc27
gevorgabgaryan <gevorg.gak@gmail.com>
2021-04-21 20:58:11 +0400

    creating info page

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express=require("express");
 
 const app=express();
 
+app.use(express.static(`public`))
+
 app.set(`view engine`,`ejs`)
 
 app.get("/",(req,res)=>{
     res.render(`index`)
 })
+
+app.get("/info",(req,res)=>{
+    data={
+        name:'Gevorg', 
+        email:'gevorgabgaryan001@gmail.com',
+        languages:['c++','javascript', 'php']
+    }
+    res.render(`info`,{data:data})
+}) 
+
 
 app.listen(3000)
