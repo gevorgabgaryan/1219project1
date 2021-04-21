8f13cc88c23d4aee7168e3a38409c6b21c66e108
gevorgabgaryan <gevorg.gak@gmail.com>
2021-04-21 21:21:02 +0400

    creating random and contact page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express=require("express");
+const random=require("random");
 
 const app=express();
 
 app.use(express.static(`public`))
 
-app.set(`view engine`,`ejs`)
+app.set(`view engine`,`ejs`);
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/",(req,res)=>{
     res.render(`index`)
@@ -17,6 +20,26 @@ app.get("/info",(req,res)=>{
         languages:['c++','javascript', 'php']
     }
     res.render(`info`,{data:data})
+}) 
+
+
+app.get("/random",(req,res)=>{
+   let rand="";
+   if(req.query.max && req.query.min){
+      rand=random.int(+req.query.min,+req.query.max)
+   }
+    res.render(`random`,{rand:rand})
+}) 
+
+
+app.get("/contact",(req,res)=>{
+    res.render(`contact`)
+}) 
+
+app.post("/contact",(req,res)=>{
+   let info=req.body;
+   console.log(info)
+    res.render(`contact-success`,{info})
 }) 
 
 
